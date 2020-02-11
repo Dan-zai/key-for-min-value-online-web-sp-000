c1f702326d8094d3e0988a8dab8d859ed7f35144
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-02-11 03:36:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,10 +5,11 @@ def key_for_min_value(name_hash)
   lowest_value = 0
   lowest_key = nil 
   
-  name_hash.each do |key, value|
+  name_hash.each { |key, value|
     if lowest_value == nil || lowest_value > value
       lowest_value = value
       lowest_key = key
-      
-  
+  }
+end
+lowest_key
   
